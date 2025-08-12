f08797d8e4cf43a26d0e8ebb174aac6ebf5a6f2e
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2025-08-12 16:24:02 +0500

    adding word document report printing

--- mainApp/mainApp/Views/RBDResults.xaml.cs
+++ mainApp/mainApp/Views/RBDResults.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
 
 namespace mainApp.Views
 {
@@ -50,7 +53,33 @@ namespace mainApp.Views
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
-        } 
+        }
         #endregion
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //WordDocument doc = new WordDocument();
+            //WSection section = doc.AddSection() as WSection;
+            //section.PageSetup.Margins.All = 72;
+            //section.PageSetup.PageSize = PageSize.A4;
+
+            //WParagraph style = doc.AddParagraphStyle("Normal") as WParagraphStyle;
+            ////style.CharacterFormat.FontName = "Calibri";
+            ////style.CharacterFormat.FontSize = 11f;
+            //style.ParagraphFormat.BeforeSpacing = 0;
+            //style.ParagraphFormat.AfterSpacing = 8;
+            //style.ParagraphFormat.LineSpacing = 13.8f;
+
+            //style = doc.AddParagraphStyle("Heading 1") as WParagraphStyle;
+            ////style.ApplyBaseStyle("Normal");
+            ////style.CharacterFormat.FontName = "Calibri Light";
+            ////style.CharacterFormat.FontSize = 16f;
+            ////style.CharacterFormat.TextColor = Color.FromArgb(46, 116, 181);
+            //style.ParagraphFormat.BeforeSpacing = 12;
+            //style.ParagraphFormat.AfterSpacing = 0;
+            //style.ParagraphFormat.Keep = true;
+            //style.ParagraphFormat.KeepFollow = true;
+            //style.ParagraphFormat.OutlineLevel = OutlineLevel.Level1;
+        }
     }
 }
