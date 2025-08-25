83ccaa807b9230b68a87f2c355c35ee966dd911c
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2025-08-25 14:26:05 +0500

    Added new project for main desk

---
+++ mainApp/ProjectDesk/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProjectDesk
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NDaF1cWGhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEBjXn1dcHVWQWReWEZ3WElfZA==");
+        }
+        protected override Window CreateShell()
+        {
+            return ContainerLocator.Container.Resolve<MainWindow>();
+            //throw new NotImplementedException();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
