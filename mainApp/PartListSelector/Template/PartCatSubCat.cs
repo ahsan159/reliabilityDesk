d2db8ad2276834e8f048a72c02e405023515887e
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2025-08-28 15:24:24 +0500

    updated controls in partlist selector

---
+++ mainApp/PartListSelector/Template/PartCatSubCat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartListSelector.Template
+{
+    public enum PartCatSubCat
+    {
+        Capactiros=0,
+        Connectors,
+        PiezoElectric,
+        Diodes,
+        Filters,
+        Fuses,
+        Inductors,
+        Microcircuits,
+        Relays,
+        Resistors,
+        Thermistors,
+        Transistors,
+        Transformers,
+        Thyristors,
+        Thermostat,
+        WiresCables,
+        Switches,
+        OptoElectronics,
+        Lamp,
+        FiberOptics,
+        RFPassive,
+        Battery,
+        Pyrotechnical,
+        Hybrids,
+        Miscellaneous
+    }
+}
